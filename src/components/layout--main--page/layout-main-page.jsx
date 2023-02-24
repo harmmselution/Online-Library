604665155797b15c9373fc3052a9bffc8ae801d0
harmmselution <bugkfbdhdh@gmail.com>
2023-02-24 03:48:50 +0300

    one more fix

--- src/components/layout--main--page/layout-main-page.jsx
+++ src/components/layout--main--page/layout-main-page.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Main } from '../../pages/main/main';
 import { NavMenu } from '../nav-menu/nav-menu';
 import '../../index.css';
 
